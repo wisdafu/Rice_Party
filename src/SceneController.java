ef70acd38bbf775d3d3c53189f0e51951bafc36e
wisdafu <wisdafu@att.net>
2017-06-21 09:56:22 -0500

    jar file

--- src/SceneController.java
+++ src/SceneController.java
@@ -34,7 +34,7 @@ public class SceneController {
         mPane.setLayout(new GridLayout(6,5));
         mFrame.add(mPane);
 
-        ricePic = new ImageIcon("res/rice.png");
+        ricePic = new ImageIcon("src/res/rice.png");
         riceBoard = new JLabel[6][5];
         for(int x = 0;x<6;x++){
             for(int y = 0;y<5;y++){
